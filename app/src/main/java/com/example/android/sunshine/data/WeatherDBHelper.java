3ba606f58f20c9719a8ce81d2416dba44645f7a8
pranavb27 <pranavbapat.pb@gmail.com>
2019-11-16 19:58:25 +0530

    Done with Share Preference Listener to update the preferred preferences by the user. Added refresh and share icons in menu Started with creating SQL Database to manage weather data locally.

---
+++ app/src/main/java/com/example/android/sunshine/data/WeatherDBHelper.java
@@ -0,0 +1,97 @@
+package com.example.android.sunshine.data;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Manages a local database for weather data.
+ */
+public class WeatherDBHelper extends SQLiteOpenHelper {
+
+
+    /*
+     * This is the name of our database. Database names should be descriptive and end with the
+     * .db extension.
+     */
+    public static final String DATABASE_NAME = "weather.db";
+    /*
+     * If you change the database schema, you must increment the database version or the onUpgrade
+     * method will not be called.
+     *
+     * The reason DATABASE_VERSION starts at 3 is because Sunshine has been used in conjunction
+     * with the Android course for a while now. Believe it or not, older versions of Sunshine
+     * still exist out in the wild. If we started this DATABASE_VERSION off at 1, upgrading older
+     * versions of Sunshine could cause everything to break. Although that is certainly a rare
+     * use-case, we wanted to watch out for it and warn you what could happen if you mistakenly
+     * version your databases.
+     */
+    private static final int DATABASE_VERSION = 3;
+
+
+    //Constructor
+    public WeatherDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+
+    /**
+     * Called when the database is created for the first time. This is where the creation of
+     * tables and the initial population of the tables should happen.
+     */
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        //Query to create table in SQL Database
+        final String SQL_WEATHER_TABLE =
+                "CREATE TABLE " + WeatherContract.WeatherEntry.TABLE_NAME + "(" +
+                        /*
+                         * WeatherEntry did not explicitly declare a column called "_ID". However,
+                         * WeatherEntry implements the interface, "BaseColumns", which does have a field
+                         * named "_ID". We use that here to designate our table's primary key.
+                         */
+                        WeatherContract.WeatherEntry._ID + "INTEGER PRIMARY KEY AUTOINCREMENT," +
+                        WeatherContract.WeatherEntry.COLUMN_DATE + "INTEGER NOT NULL, " +
+                        WeatherContract.WeatherEntry.COLUMN_WEATHER_ID + "INTEGER NOT NULL, " +
+                        WeatherContract.WeatherEntry.COLUMN_MIN_TEMP + "REAL NOT NULL, " +
+                        WeatherContract.WeatherEntry.COLUMN_MAX_TEMP + "REAL NOT NULL, " +
+                        WeatherContract.WeatherEntry.COLUMN_HUMIDITY + "REAL NOT NULL, " +
+                        WeatherContract.WeatherEntry.COLUMN_PRESSURE + "REAL NOT NULL, " +
+                        WeatherContract.WeatherEntry.COLUMN_WIND_SPEED + "REAL NOT NULL," +
+                        WeatherContract.WeatherEntry.COLUMN_DEGREES + "REAL NOT NULL, " +
+                        /*
+                         * To ensure this table can only contain one weather entry per date, we declare
+                         * the date column to be unique. We also specify "ON CONFLICT REPLACE". This tells
+                         * SQLite that if we have a weather entry for a certain date and we attempt to
+                         * insert another weather entry with that date, we replace the old weather entry.
+                         */
+                        " UNIQUE ( " + WeatherContract.WeatherEntry.COLUMN_DATE + ") ON CONFLICT REPLACE );";
+
+        //Execute the Query to create the database
+        sqLiteDatabase.execSQL(SQL_WEATHER_TABLE);
+
+
+    }
+
+    /**
+     * This database is only a cache for online data, so its upgrade policy is simply to discard
+     * the data and call through to onCreate to recreate the table. Note that this only fires if
+     * you change the version number for your database (in our case, DATABASE_VERSION). It does NOT
+     * depend on the version number for your application found in your app/build.gradle file. If
+     * you want to update the schema without wiping data, commenting out the current body of this
+     * method should be your top priority before modifying this method.
+     *
+     * @param sqLiteDatabase Database that is being upgraded
+     * @param oldVersion     The old database version
+     * @param newVersion     The new database version
+     */
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        //Drop Table if Exists
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + WeatherContract.WeatherEntry.TABLE_NAME);
+        //Pass the sq lite database to onCreate
+        onCreate(sqLiteDatabase);
+
+    }
+}
---
+++ app/src/main/java/com/example/android/sunshine/data/WeatherProvider.java
@@ -0,0 +1,93 @@
+package com.example.android.sunshine.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+public class WeatherProvider extends ContentProvider {
+    /*
+     * These constant will be used to match URIs with the data they are looking for. We will take
+     * advantage of the UriMatcher class to make that matching MUCH easier than doing something
+     * ourselves, such as using regular expressions.
+     */
+    private final static int CODE_WEATHER = 100;
+    private final static int CODE_WEATHER_WITH_DATE = 101;
+    private WeatherDBHelper mOpenHelper;
+
+    /**
+     * Creates the UriMatcher that will match each URI to the CODE_WEATHER and
+     * CODE_WEATHER_WITH_DATE constants defined above.
+     * UriMatcher does all the hard work for you. You just have to tell it which code to match
+     * with which URI, and it does the rest automagically. Remember, the best programmers try
+     * to never reinvent the wheel. If there is a solution for a problem that exists and has
+     * been tested and proven, you should almost always use it unless there is a compelling
+     * reason not to.
+     *
+     * @return A UriMatcher that correctly matches the constants for CODE_WEATHER and CODE_WEATHER_WITH_DATE
+     */
+
+    public static UriMatcher buildUriMatcher() {
+        /*
+         * All paths added to the UriMatcher have a corresponding code to return when a match is
+         * found. The code passed into the constructor of UriMatcher here represents the code to
+         * return for the root URI. It's common to use NO_MATCH as the code for this case.
+         */
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = WeatherContract.CONTENT_AUTHORITY;
+        /* Build uri matcher */
+        return matcher;
+    }
+
+    /**
+     * In onCreate, we initialize our content provider on startup. This method is called for all
+     * registered content providers on the application main thread at application launch time.
+     * It must not perform lengthy operations, or application startup will be delayed.
+     * <p>
+     * Nontrivial initialization (such as opening, upgrading, and scanning
+     * databases) should be deferred until the content provider is used (via {@link #query},
+     * {@link #bulkInsert(Uri, ContentValues[])}, etc).
+     * <p>
+     * Deferred initialization keeps application startup fast, avoids unnecessary work if the
+     * provider turns out not to be needed, and stops database errors (such as a full disk) from
+     * halting application launch.
+     *
+     * @return true if the provider was successfully loaded, false otherwise
+     */
+    @Override
+    public boolean onCreate() {
+        mOpenHelper = new WeatherDBHelper(getContext());
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] strings, @Nullable String s, @Nullable String[] strings1, @Nullable String s1) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues contentValues, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+}
